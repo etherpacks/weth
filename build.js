26870d0eed66b100fe63f7b13500804e86771bb6
stobiewan <getsforwarded@gmail.com>
2022-01-11 21:19:14 +1300

    get packs building
    use artifacts for deploying in mock task

--- build.js
+++ build.js
@@ -1,32 +1,39 @@
 // assemble packs for existing deployments
 
+const fs = require('fs')
+
 const dpack = require('dpack')
 
-// Downloaded from X at T time
-// Verified by A, B, C...
-const weth9_artifact = require('link/weth-artifact.json')
+// Artifacts compiled from etherscan data on 11 Jan 22
+// Verified with remix
+const weth9_artifacts = {
+  'ethereum': require('./link/weth-ethereum-artifact.json'),
+  'ropsten': require('./link/weth-ropsten-artifact.json'),
+  'kovan': require('./link/weth-kovan-artifact.json'),
+  'goerli': require('./link/weth-goerli-artifact.json')
+}
 
 const weth9_addresses = {
-  'ethereum': '', // Verify -- it's #2 ETH holder after ETH2 deposit contract
-  'ropsten': '',
-  'kovan': '',
-  'goerli': '',
+  'ethereum': '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // Verify -- it's #2 ETH holder after ETH2 deposit contract, https://tokenlists.org/
+  'ropsten': '0xc778417E063141139Fce010982780140Aa0cD5Ab', // Verify -- https://tokens.uniswap.org and etherscan, 290k ropsten eth
+  'kovan': '0xd0A1E359811322d97991E03f863a0C30C2cF029C', // Verify -- https://tokens.uniswap.org and etherscan, 720k kovan eth
+  'goerli': '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', // Verify -- https://tokens.uniswap.org and etherscan, 250k goerli eth
 }
 
 async function build(network) {
   const builder = new dpack.PackBuilder(network)
   await builder.packObject({
-    objectname: 'weth9'
+    objectname: 'weth9',
     address: weth9_addresses[network],
     typename: 'WETH9',
-    artifact: weth9_artifact
+    artifact: weth9_artifacts[network]
   }, true) // alsoPackType
   const pack = await builder.build();
 
-  fs.writeFileSync(`pack/weth9_${network}.dpack.json`, JSON.stringify(pack, null, 2));
+  fs.writeFileSync(`./pack/weth9_${network}.dpack.json`, JSON.stringify(pack, null, 2));
 }
 
 build('ethereum')
 build('ropsten')
 build('kovan')
-
+build('goerli')
