c750d7958f12522501ec9f721a8dac741c6b714c
nikolai <mail@nikolai.fyi>
2022-01-31 16:18:18 -0400

    fix mock deploy

--- task/deploy-mock-weth.js
+++ task/deploy-mock-weth.js
@@ -7,14 +7,14 @@ const { task } = require('hardhat/config')
 task('deploy-mock-weth', 'deploy mock weth')
 .setAction(async (args, hre) => {
   const [ signer ]  = await hre.ethers.getSigners();
-  const pack = require('../packs/weth_ethereum.dpack.json')  // reference deployment for mocks
+  const pack = require('../pack/weth_ethereum.dpack.json')  // reference deployment for mocks
   const dapp = await dpack.Dapp.loadFromPack(pack, signer, hre.ethers)
   const weth = await dapp.types.WETH9.deploy()
   const mockpack = JSON.parse(JSON.stringify(pack))
   mockpack.network = hre.network.name
   mockpack.objects.weth.address = weth.address;
   mockpack.objects.weth9.address = weth.address;
-  const mockpath = path.join(__dirname, `../packs/weth_${hre.network.name}.dpack.json`)
+  const mockpath = path.join(__dirname, `../pack/weth_${hre.network.name}.dpack.json`)
   const mockjson = JSON.stringify(mockpack, null, 2)
   fs.writeFileSync(mockpath, mockjson);
   return mockpack;
