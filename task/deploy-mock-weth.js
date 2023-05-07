3ef51e8fd945f9fe1a5142cb2b03654c5b2d8421
kbrav <>
2023-05-06 20:33:38 -0400

    netname

--- task/deploy-mock-weth.js
+++ task/deploy-mock-weth.js
@@ -6,7 +6,8 @@ const { task } = require('hardhat/config')
 
 task('deploy-mock-weth', 'deploy mock weth')
 .setAction(async (args, hre) => {
-  const pack = require('../pack/weth_ethereum.dpack.json')
+  const netname = hre.network.name.replace('_fork', '')
+  const pack = require(`../pack/weth_${netname}.dpack.json`)
   const [ signer ]  = await hre.ethers.getSigners()
   const dapp = await dpack.load(pack, hre.ethers, signer)
   const weth = await dapp._types.WETH9.deploy()
@@ -14,7 +15,7 @@ task('deploy-mock-weth', 'deploy mock weth')
   mockpack.network = hre.network.name
   mockpack.objects.weth.address = weth.address;
   mockpack.objects.weth9.address = weth.address;
-  const mockpath = path.join(__dirname, `../pack/weth_${hre.network.name}.dpack.json`)
+  const mockpath = path.join(__dirname, `../pack/weth_${netname}.dpack.json`)
   const mockjson = JSON.stringify(mockpack, null, 2)
   fs.writeFileSync(mockpath, mockjson);
   return mockpack;
